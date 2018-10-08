3e7748122c047a6f1e94e35d3147fb5f7d37c42d
pjaudiomv <pjaudiomv@gmail.com>
2018-10-08 19:39:23 -0400

    2 commit

--- bmlt-zip-county-geocode.php
+++ bmlt-zip-county-geocode.php
@@ -81,7 +81,7 @@ function get($url) {
     error_log($url);
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/4.0 (compatible; MSIE 5.01; Windows NT 5.0) +nerna' );
+    curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/4.0 (compatible; MSIE 5.01; Windows NT 5.0) +bmltgeo' );
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     $data = curl_exec($ch);
     $errorno = curl_errno($ch);
