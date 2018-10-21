3610055bf60d4e473ca3a031e206e3033ca05ecd
pjaudiomv <pjaudiomv@gmail.com>
2018-10-21 11:18:34 -0400

    dont log curl calls

--- bmlt-zip-county-geocode.php
+++ bmlt-zip-county-geocode.php
@@ -64,7 +64,7 @@ function getDetailsForAddress($address) {
 }
 
 function get($url) {
-    error_log($url);
+    //error_log($url);
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/4.0 (compatible; MSIE 5.01; Windows NT 5.0) +bmltgeo' );
